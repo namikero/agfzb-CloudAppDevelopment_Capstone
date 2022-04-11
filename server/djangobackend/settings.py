1770bf037fb2259f3d9d3979026dc0f2c6199134
Mark Joseph Gonzales <mjgonzales.1982@gmail.com>
2022-04-11 09:04:15 +0000

    Adding final 3 changes to Github

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", "azmon08.mybluemix.net"]
+ALLOWED_HOSTS = ["localhost", "azmon08.us-south.cf.appdomain.cloud"]
 
 
 # Application definition
